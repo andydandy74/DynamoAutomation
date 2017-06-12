aa0186d5c60a013675da685bfa707de7721b0b51
Andreas Dieckmann <adieckmann@gmp-architekten.de>
2017-06-12 09:43:23 +0200

    Testing new journal keys for 2017

--- src/ProcessRunner/Journal.cs
+++ src/ProcessRunner/Journal.cs
@@ -15,10 +15,7 @@ namespace Dynamo.Automation
         /// <summary>
         /// Create a journal file for executing a Dynamo workspace on a Revit file.
         /// 
-        /// This journal file uses three keys specifically for the purpose of automating Dynamo.
-        /// dynPath - Specifies the path to the Dynamo workspace to execute.
-        /// dynShowUI - Specifies whether the Dynamo UI should be visible (set to false - Dynamo will run headless).
-        /// dynAutomation - Specifies that Dynamo should run in 'automation' mode. This mode is similar to testing in that
+        /// This journal file uses several keys specifically for the purpose of automating Dynamo.
         /// Dynamo is never run in the idle loop. The workspace is executed immediately, and control is returned to the DynamoRevit
         /// external application.
         /// </summary>
@@ -44,24 +41,28 @@ namespace Dynamo.Automation
             // Finding the installed version of Dynamo Revit
             // Successfully stole this part from Dynamo StartupUtils.cs and modified it as needed :-)
             const string regKey64 = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\";
-            //Open HKLM for 64bit registry
+            // Open HKLM for 64bit registry
             var regKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
-            //Open Windows/CurrentVersion/Uninstall registry key
+            // Open Windows/CurrentVersion/Uninstall registry key
             regKey = regKey.OpenSubKey(regKey64);
-            //Get "Version" value as string for all the subkeys that start with "Dynamo Revit"
+            // Get "Version" value as string for all the subkeys that start with "Dynamo Revit"
             var installedVersions = regKey.GetSubKeyNames().Where(s => s.StartsWith("Dynamo Revit")).Select((s) => regKey.OpenSubKey(s).GetValue("Version") as string);
             string dynVersion = installedVersions.First().Substring(0, 3);
             // This code is not bothering to check if any pre-1.0 Dynamo is installed - lazy...
 
             // Launch command to be used depends on Revit & Dynamo versions
             string launchCommand;
+            string launchArgs;
+            // Launch command and journal keys differ between pre-2017 and post-2016 Revit
             if (revitVersion > 2016)
             {
                 launchCommand = "Jrn.Command \"Ribbon\" , \"Launch Dynamo, ID_VISUAL_PROGRAMMING_DYNAMO\" \n";
+                launchArgs = String.Format("Jrn.Data \"APIStringStringMapJournalData\", 5, \"dynPath\", \"{0}\", \"dynShowUI\", \"true\", \"dynAutomation\", \"false\", \"dynPathExecute\", \"true\", \"dynModelShutDown\", \"true\" \n", workspacePath.Replace(' ', '/'));
             }
             else
             {
                 launchCommand = String.Format("Jrn.RibbonEvent \"Execute external command:CustomCtrl_%CustomCtrl_%Add-Ins%Visual Programming%Dynamo {0}:Dynamo.Applications.DynamoRevit\" \n", dynVersion);
+                launchArgs = String.Format("Jrn.Data \"APIStringStringMapJournalData\", 3, \"dynPath\", \"{0}\", \"dynShowUI\", \"false\", \"dynAutomation\", \"true\" \n", workspacePath.Replace(' ', '/'));
             }
 
             // Create journal file
@@ -71,13 +72,13 @@ namespace Dynamo.Automation
                                             "Dim Jrn \n" +
                                             "Set Jrn = CrsJournalScript \n" +
                                             "Jrn.Command \"StartupPage\" , \"Open this project , ID_FILE_MRU_FIRST\" \n" +
-                                            "Jrn.Data \"MRUFileName\"  , \"{0}\" \n{1}" +
-                                            "Jrn.Data \"APIStringStringMapJournalData\", 3, \"dynPath\", \"{2}\", \"dynShowUI\", \"false\", \"dynAutomation\", \"true\" \n" +
+                                            "Jrn.Data \"MRUFileName\"  , \"{0}\" \n" +
+                                            "{1}" +
+                                            "{2}" +
                                             "Jrn.Command \"Internal\" , \"Flush undo and redo stacks , ID_FLUSH_UNDO\" \n" +
                                             "Jrn.Command \"Internal\" , \"Close the active project , ID_REVIT_FILE_CLOSE\" \n" +
                                             "Jrn.Command \"SystemMenu\" , \"Quit the application; prompts to save projects , ID_APP_EXIT\"",
-                    revitFilePath.Replace(' ', '/'), launchCommand,  workspacePath.Replace(' ', '/'));
-
+                    revitFilePath.Replace(' ', '/'), launchCommand,  launchArgs);
                 tw.Write(journal);
                 tw.Flush();
             }
